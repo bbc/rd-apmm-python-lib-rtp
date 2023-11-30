3ea3fe6d6df3033e9bbe1232454204b8f5699fe8
James Sandford <james.sandford@bbc.co.uk>
2023-11-30 13:56:35 +0000

    Add github actions ci workflow

--- rtp/csrcList.py
+++ rtp/csrcList.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
 from collections import UserList
 from typing import Iterable, List, Union
 from .errors import LengthError
